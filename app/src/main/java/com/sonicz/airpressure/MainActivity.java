e4e74b113f6baf62ac512f2da28f00eb18ae5895
sonicprogrammer <sonichunter@sonichunter.tk>
2016-02-29 21:20:48 +1100

    Added average reading.

--- app/src/main/java/com/sonicz/airpressure/MainActivity.java
+++ app/src/main/java/com/sonicz/airpressure/MainActivity.java
@@ -9,18 +9,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
-    TextView text;
+    TextView text, text2;
     android.hardware.SensorManager sm;
     Sensor pre;
     double pressure = 0;
+
+    double sum = 0;
+    ArrayBlockingQueue<Double> q = new ArrayBlockingQueue(50);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView);
+        text2 = (TextView) findViewById(R.id.textView2);
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         pre = sm.getDefaultSensor(Sensor.TYPE_PRESSURE);
         sm.registerListener( this, pre, SensorManager.SENSOR_DELAY_UI);
@@ -30,8 +36,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         pressure = event.values[0];
-        text.setText("Current pressure:\n" + String.format("%.1f", pressure) + " hPa\n");
+        text.setText("Current:\n" + String.format("%.1f", pressure) + " hPa\n");
         text.append(String.format("%.3f", hPa2inHg(pressure)) + " inHg");
+
+        q.add(pressure);
+        sum += pressure;
+        if (q.size() >= 50) {
+            sum -= q.poll();
+            double avg = sum / q.size();
+            text2.setText("50 Average:\n" + String.format("%.1f", avg) + " hPa\n");
+            text2.append(String.format("%.3f", hPa2inHg(avg)) + " inHg");
+        }
     }
 
     @Override
@@ -41,5 +56,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public double hPa2inHg(double hPa) {
         return hPa * 0.02952998751;
     }
+
+
 
 }
